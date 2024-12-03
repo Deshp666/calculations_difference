db841ca6f3b8f69615d458183dc8929d87262d65
Deshp666 <motvey74@mail.ru>
2024-12-03 22:57:50 +0500

    refactoring

--- task 2/async requests.py
+++ task 2/async requests.py
@@ -1,10 +1,12 @@
+from asyncio import timeout
+
 import httpx
 import asyncio
 import time
 
 
 async def fetch_url(client, url):
-    response = await client.get(url)
+    response = await client.get(url, timeout=60)
     return response.text
 
 
@@ -14,7 +16,7 @@ async def fetch_all(urls):
         return await asyncio.gather(*tasks)
 
 async def main():
-    urls = ["https://jsonplaceholder.typicode.com/posts/1"] * 150
+    urls = ["https://jsonplaceholder.typicode.com/posts/55"] * 150
     start_time = time.time()
     results = await fetch_all(urls)
     print(f"Fetched {len(results)} pages.")
